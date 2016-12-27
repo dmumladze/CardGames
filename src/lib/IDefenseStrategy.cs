93f7e89787dcfec2b40fa9fcc97c2a157b3ecbb2
Mumladze <dm32998@nsroot.net>
2016-12-27 17:11:39 -0500

    corrected sdk version

--- src/lib/IDefenseStrategy.cs
+++ src/lib/IDefenseStrategy.cs
@@ -5,6 +5,6 @@ namespace CardGames.Lib
 {
     public interface IDefenseStrategy<T> 
     {
-        List<Trick<T>> Execute(CardCollection<T> deffenderCards, CardCollection<T> attackerCards);
+        List<Trick<T>> Execute(CardCollection<T> defenderCards, CardCollection<T> attackerCards);
     }
 }
